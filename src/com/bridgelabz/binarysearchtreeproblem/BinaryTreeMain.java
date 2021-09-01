d7cdaffa2b5cc9de601d4e5cf2cbfc042c7138b2
Vinay288 <vinayhiremath288@gmail.com>
2021-09-01 17:42:46 +0530

    ADDED:function call to add method and size method

--- src/com/bridgelabz/binarysearchtreeproblem/BinaryTreeMain.java
+++ src/com/bridgelabz/binarysearchtreeproblem/BinaryTreeMain.java
@@ -3,7 +3,11 @@ package com.bridgelabz.binarysearchtreeproblem;
 public class BinaryTreeMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BinaryTree<Integer> binaryTree  = new BinaryTree<Integer>();
+		binaryTree.add(20);
+		binaryTree.add(10);
+		binaryTree.add(30);
+		System.out.println(binaryTree.size());
 
 	}
 
