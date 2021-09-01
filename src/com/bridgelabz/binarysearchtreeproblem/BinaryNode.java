a98a57c4c3e1f4f6eeaee9693bc78b16a50b4b48
Vinay288 <vinayhiremath288@gmail.com>
2021-09-01 17:41:33 +0530

    ADDED:constructor to initlaize key ,left and right

--- src/com/bridgelabz/binarysearchtreeproblem/BinaryNode.java
+++ src/com/bridgelabz/binarysearchtreeproblem/BinaryNode.java
@@ -1,5 +1,13 @@
 package com.bridgelabz.binarysearchtreeproblem;
 
-public class BinaryNode {
+public class BinaryNode<K extends Comparable<K>> {
+	K key;
+	BinaryNode<K> left, right;
+
+	public BinaryNode(K key) {
+		this.key = key;
+		this.left = null;
+		this.right = null;
+	}
 
 }
